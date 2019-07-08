9c003094248cda2115cf5c0ad01eefc67d13f7ec
Kai Krings <kai.krings@posteo.de>
2019-07-08 15:46:32 +0200

    Add a setup file.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+
+setuptools.setup(
+    name="pyslurm",
+    version="0.1",
+    author="Kai Krings",
+    author_email="kai.krings@posteo.net",
+    description="Simple Python interface to Slurm",
+    url="https://github.com/kkrings/pyslurm/",
+    license="GPLv3",
+    packages=setuptools.find_packages())
