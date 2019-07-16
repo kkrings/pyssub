1c1dfeb6d244fc4f59578102214153d7629f889f
Kai Krings <kai.krings@posteo.de>
2019-07-16 13:54:21 +0200

    Move the submission routine to the pysbatch script.

--- pyslurm/sbatch.py
+++ pyslurm/sbatch.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Module containing a class for submitting Slurm batch scripts
+"""Module containing a function for submitting Slurm batch scripts
 
 """
 import configparser
@@ -8,149 +8,94 @@ import os
 import re
 import subprocess
 import tempfile
-import time
 
 from . import sbatchscript
 
 
 # ---Submitting Slurm batch scripts--------------------------------------------
-class SBatch:
-    """Slurm batch script submitter
+def submit(script, partition=None):
+    """Submit Slurm batch script.
 
-    This class allows the successive submission of a collection of
-    scripts into the Slurm queue, checking that not more jobs
-    than `nmax` are queuing.
-
-    Attributes
+    Parameters
     ----------
-    nmax : int
-        Maximum allowed number of queuing jobs
-    wait : int
-        Number of seconds before next submission
+    script : SBatchScript
+        Slurm batch script
+    partition : str, optional
+        Partition for resource allocation
+
+    Returns
+    -------
+    int
+        Job ID
 
     """
-    def __init__(self, nmax=1000, wait=120):
-        self.nmax = nmax
-        self.wait = wait
+    # Save script to disk.
+    options = {
+        "mode": "w",
+        "prefix": "pyslurm_",
+        "suffix": ".sh",
+        "delete": False
+        }
 
-    def __call__(self, scripts, partition=None):
-        """Successively submit Slurm batch scripts.
+    with tempfile.NamedTemporaryFile(**options) as scriptfile:
+        scriptfile.write(str(script))
 
-        Parameters
-        ----------
-        scripts : dict(str, SBatchScript)
-            Mapping of job names to Slurm batch scripts
-        partition : str, optional
-            Partition for resource allocation
+    command = ["sbatch"]
 
-        Returns
-        -------
-        dict(str, int)
-            Mapping of job names to job IDs
+    if partition is not None:
+        command.extend(["-p", partition])
 
-        """
-        # We do not want to change the given dictionary.
-        scripts = dict(scripts)
+    command.append(scriptfile.name)
 
-        jobs = {}
-        while len(scripts) > 0:
-            nnew = min(
-                self.nmax - self.njobs(str(os.getuid()), partition),
-                len(scripts))
+    process = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+        encoding="utf-8")
 
-            for _ in range(nnew):
-                name, script = scripts.popitem()
-                jobs[name] = self.submit(script, partition)
+    match = re.match(
+        r"Submitted batch job (?P<jobid>\d+)",
+        process.stdout)
 
-            if len(scripts) > 0:
-                time.sleep(self.wait)
+    # Remove script from disk.
+    os.remove(scriptfile.name)
 
-        return jobs
+    return int(match.group("jobid"))
 
-    @staticmethod
-    def submit(script, partition=None):
-        """Submit Slurm batch script.
 
-        Parameters
-        ----------
-        script : SBatchScript
-            Slurm batch script
-        partition : str, optional
-            Partition for resource allocation
+def njobs(user, partition=None):
+    """Number of queuing jobs
 
-        Returns
-        -------
-        int
-            Job ID
+    Check the number of queuing jobs for the given `user`
+    and `partition`.
 
-        """
-        # Save script to disk.
-        options = {
-            "mode": "w",
-            "prefix": "pyslurm_",
-            "suffix": ".sh",
-            "delete": False
-            }
+    Parameters
+    ----------
+    user : str
+        User name or ID
+    partition : str, optional
+        Partition name
 
-        with tempfile.NamedTemporaryFile(**options) as scriptfile:
-            scriptfile.write(str(script))
+    Returns
+    -------
+    int
+        Number of queuing jobs
 
-        command = ["sbatch"]
+    """
+    command = ["squeue", "-h", "-o", "'%.i'", "-u", user]
 
-        if partition is not None:
-            command.extend(["-p", partition])
+    if partition is not None:
+        command.extend(["-p", partition])
 
-        command.append(scriptfile.name)
+    process = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+        encoding="utf-8")
 
-        process = subprocess.run(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            check=True,
-            encoding="utf-8")
-
-        match = re.match(
-            r"Submitted batch job (?P<jobid>\d+)",
-            process.stdout)
-
-        # Remove script from disk.
-        os.remove(scriptfile.name)
-
-        return int(match.group("jobid"))
-
-    @staticmethod
-    def njobs(user, partition=None):
-        """Number of queuing jobs
-
-        Check the number of queuing jobs for the given `user`
-        and `partition`.
-
-        Parameters
-        ----------
-        user : str
-            User name or ID
-        partition : str, optional
-            Partition name
-
-        Returns
-        -------
-        int
-            Number of queuing jobs
-
-        """
-        command = ["squeue", "-h", "-o", "'%.i'", "-u", user]
-
-        if partition is not None:
-            command.extend(["-p", partition])
-
-        process = subprocess.run(
-            command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            check=True,
-            encoding="utf-8")
-
-        return len(process.stdout.splitlines())
+    return len(process.stdout.splitlines())
 
 
 # --Loading Slurm batch scripts------------------------------------------------
