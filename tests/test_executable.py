7998ad8c35aaeb6404d5d483eaaba71117c445a0
Kai Krings <kai.krings@posteo.de>
2019-07-30 12:09:28 +0200

    Add first unit tests.

---
+++ tests/test_executable.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test executable for PySlurm.
+
+If no command-line arguments are provided, this script simulates the
+successful execution of a command. A failed execution is simulated by
+raising a runtime exception if the option fail is used. This script can
+also be used to test the simple file transfer mechanism provided by
+PySlurm by either reading the content of the given input file or by
+writing an output file.
+
+"""
+
+
+def main(inputfile=None, outputfile=None, fail=False):
+    if fail:
+        raise RuntimeError("Simulate a failed execution.")
+
+    print("Simulate a successful execution.")
+
+    if inputfile is not None:
+        print("Read test input file.")
+
+        with open(inputfile) as stream:
+            print(stream.readlines()[0][:-1])
+
+    if outputfile is not None:
+        print("Write a test output file.")
+
+        with open(outputfile, "w") as stream:
+            stream.write("This is a test output file for PySlurm.")
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        "--in",
+        nargs="?",
+        type=str,
+        help="path to test input file",
+        metavar="PATH",
+        dest="inputfile")
+
+    parser.add_argument(
+        "--out",
+        nargs="?",
+        type=str,
+        help="path to test output file",
+        metavar="PATH",
+        dest="outputfile")
+
+    parser.add_argument(
+        "--fail",
+        help="raise a runtime exception",
+        action="store_true",
+        default=False)
+
+    args = parser.parse_args()
+
+    main(args.inputfile, args.outputfile, args.fail)
---
+++ tests/test_sbatch.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Unit tests for sbatch module.
+
+"""
+import os
+import subprocess
+import tempfile
+import unittest
+
+import pyslurm.sbatch
+
+
+class TestSBatchScriptMacro(unittest.TestCase):
+    """Test case for sbatch module
+
+    Execute the test executable locally within a batch script; test for
+    a return code of zero, an existing output file, and a proper cleanup
+    of the working directory.
+
+    """
+    @classmethod
+    def setUpClass(cls):
+        # Create temporary working directory.
+        cls.workdir = tempfile.TemporaryDirectory(prefix="pyslurm_")
+
+        # Create test input file.
+        inputfile = os.path.join(cls.workdir.name, "test_input.txt")
+
+        with open(inputfile, "w") as stream:
+            stream.write("This is a test input file for PySlurm.\n")
+
+        # Create batch script.
+        script = pyslurm.sbatch.SBatchScript(
+            executable=os.path.abspath("test_executable.py"),
+            arguments="--in test_input.txt --out test_output.txt")
+
+        script.transfer_executable = True
+        script.transfer_input_files.append("{macros[inputfile]}")
+        script.transfer_output_files.append("{macros[outputfile]}")
+
+        cls.outputfile = os.path.join(cls.workdir.name, "test_output.txt")
+
+        macros = {
+            "inputfile": inputfile,
+            "outputfile": cls.outputfile
+            }
+
+        script = pyslurm.sbatch.SBatchScriptMacro(script, macros)
+        scriptfile = os.path.join(cls.workdir.name, "test_executable.sh")
+
+        with open(scriptfile, "w") as stream:
+            stream.write(str(script))
+
+        # Execute batch script
+        cls.process = subprocess.run(
+            ["bash", scriptfile],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=cls.workdir.name,
+            check=False,
+            encoding="utf-8")
+
+    @classmethod
+    def tearDownClass(cls):
+        # Remove temporary working directory.
+        cls.workdir.cleanup()
+
+    def test_return_code(self):
+        """Test for return code of zero.
+
+        """
+        message = "Batch script failed with return code {}: {}.".format(
+            self.process.returncode, self.process.stderr)
+
+        self.assertEqual(self.process.returncode, 0, message)
+
+    def test_output_file(self):
+        """Test for existing output file.
+
+        """
+        message = "Output file {} does not exist.".format(self.outputfile)
+        self.assertTrue(os.path.exists(self.outputfile), message)
+
+    def test_cleanup(self):
+        """Test for proper cleanup of working directory.
+
+        """
+        workdir = os.path.join(self.workdir.name, "slurm")
+        message = "Working directory {} was not removed.".format(workdir)
+        self.assertFalse(os.path.exists(workdir), message)
+
+
+if __name__ == "__main__":
+    # Run unit test.
+    unittest.main()
