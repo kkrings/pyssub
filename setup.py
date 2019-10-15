03a729aa01917b1da3ab6f58eb2ecc1c88bc76e8
Kai Krings <kai.krings@posteo.de>
2019-10-15 16:31:05 +0200

    This project also works with Python 3.7.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: Unix",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering"
         ],
     python_requires=">=3.6",
